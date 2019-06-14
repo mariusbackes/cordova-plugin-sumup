7a2edba806a26a6254a564f40fde7cf12e43d60d
mariusbackes <mariusbackes@icloud.com>
2019-06-15 01:21:20 +0200

    fix swift callback result

--- src/ios/SumUp.swift
+++ src/ios/SumUp.swift
@@ -7,43 +7,43 @@
 
     @objc(auth:)
     func auth(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["code": 112, "message": "Auth method will be implemented soon"] as [AnyHashable : Any]);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["code": 112, "message": "Auth method will be implemented soon"] as [AnyHashable : Any]);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
 
     @objc(getSettings:)
     func getSettings(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["code": 7, "message": "Get settings method will be implemented soon"] as [AnyHashable : Any]);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["code": 7, "message": "Get settings method will be implemented soon"] as [AnyHashable : Any]);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
 
     @objc(logout:)
     func logout(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["code": 103, "message": "Logout method will be implemented soon"] as [AnyHashable : Any]);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["code": 103, "message": "Logout method will be implemented soon"] as [AnyHashable : Any]);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
 
     @objc(isLoggedIn:)
     func isLoggedIn(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["code": 102, "message": "Is logged in method will be implemented soon"] as [AnyHashable : Any]);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["code": 102, "message": "Is logged in method will be implemented soon"] as [AnyHashable : Any]);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
 
     @objc(prepare:)
     func prepare(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["code": 109, "message": "Prepare method will be implemented soon"] as [AnyHashable : Any]);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["code": 109, "message": "Prepare method will be implemented soon"] as [AnyHashable : Any]);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
 
     @objc(closeConnection:)
     func closeConnection(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["code": 104, "message": "Close connection method will be implemented soon"] as [AnyHashable : Any]);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["code": 104, "message": "Close connection method will be implemented soon"] as [AnyHashable : Any]);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
 
     @objc(pay:)
     func pay(command: CDVInvokedUrlCommand) {
-        let pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: ["code": 117, "message": "Pay method will be implemented soon"] as [AnyHashable : Any]);
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["code": 117, "message": "Pay method will be implemented soon"] as [AnyHashable : Any]);
         self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
     }
 }
